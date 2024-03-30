2b2aadb1dff021227acf7d505b1eb8ac89822ac7
SuelenGalhardo <suelenranucci@gmail.com>
2024-03-30 12:11:05 +0100

    added refactoring

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
+//variaveis
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
 const randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
-//funcao callback (chamala de volta)
+
+//eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+      handleResetClick()
+  }
+})
+
+//funcao callback (chama ela de volta)
 
 function handleTryClick(event) {
   event.preventDefault(); //nao faca o padrao
-
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
-
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `acertou em ${xAttempts} tentativas`;
+ toggleScreem() 
+ 
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`;
   }
   inputNumber.value = ''
   xAttempts++
 }
 
-//eventos
-
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
+function handleResetClick () {
+  toggleScreem()
   xAttempts = 1
-
+  randomNumber = Math.round(Math.random() * 10) //reseta o handomNumber também
 }
 
-);
+function toggleScreem () {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
